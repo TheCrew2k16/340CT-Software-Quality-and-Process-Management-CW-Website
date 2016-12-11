f830658b508ad51b1cc0ddf8eb3630da5571796e
Philip Bell <philhabell@gmail.com>
2016-12-11 03:21:38 +0000

    Fixed makeDatabase.php will now create DB
    note: file will overwrite any db called "cwdb" so backup any db you have
    with that name already/ dont run it again after altering you db.

--- includes/dbconnect.php
+++ includes/dbconnect.php
@@ -5,6 +5,9 @@ $pass="";
 
 
 $conn = mysql_connect($server,$user,$pass);
+if(!$conn){
+    die("Could not connect: ". mysql_error());
+}
 //if ($conn->connect_error){
 //    die("Connection failed: ".$conn->connect_error);
 //}
--- makeDatabase.php
+++ makeDatabase.php
@@ -2,11 +2,51 @@
 //this file will create the database and make the tables, navigate to "localhost/wevsite/makeDatabase.php" to run it
 include "includes\dbconnect.php";
 
-mysql_query("CREATE DATABASE cwdb");
+mysql_query("DROP DATABASE cwdb");
+mysql_query("CREATE DATABASE cwdb",$conn)or die("Could not add cwdb database: ".mysql_error());
 
 mysql_select_db("cwdb");
 
-mysql_query("CREATE TABLE logintable(student_ID INT NOT NULL AUTO_INCREMENT,forename VARCHAR(50) NOT NULL,surename VARCHAR(50) NOT NULL,PRIMARY KEY(student_ID))");//or die(mysql_error);
-mysql_query("CREATE TABLE submissionTable(student_ID INT NOT NULL AUTO_INCREMENT,coursework_ID INT NOT NULL AUTO_INCREMENT, submission_status BIT NOT NULL, mark INT, moderated BIT NOT NULL,submission_date DATE, PRIMARRY KEY(student_ID)) ");// or die(mysql_error);
-mysql_query("CREATE TABLE cwtable(coursework_ID INT NOT BULL AUTO_INCREMENT,coursework_title VARCHAR(50) NOT NULL,coursework_name VARCHAR(50) NOT NULL,module_ID VARCHAR(50) NOT NULL, issue_date VARCHAR(50) NOT NULL, deadline VARCHAR(50) NOT NULL,PRIMARY KEY(coursework_ID))"); //or die(mysql_error);
+mysql_query("CREATE TABLE logintable(".
+"student_ID INT NOT NULL AUTO_INCREMENT,".
+"forename VARCHAR(50) NOT NULL,".
+"surename VARCHAR(50) NOT NULL,".
+"PRIMARY KEY(student_ID))")or die("Could not add logintable: ".mysql_error());
 
+mysql_query("CREATE TABLE cwtable(".
+"coursework_ID INT NOT NULL AUTO_INCREMENT,".
+"coursework_title VARCHAR(50) NOT NULL,".
+"coursework_name VARCHAR(50) NOT NULL,".
+"module_ID VARCHAR(50) NOT NULL,".
+"issue_date VARCHAR(50) NOT NULL,".
+"deadline VARCHAR(50) NOT NULL,".
+"PRIMARY KEY(coursework_ID))")or die("Could not add cwtable: ".mysql_error());
+
+mysql_query("CREATE TABLE submissiontable(".
+"submission_ID INT NOT NULL AUTO_INCREMENT,".
+"student_ID INT NOT NULL,".
+"coursework_ID INT NOT NULL,".
+"mark INT,".
+"moderated BIT NOT NULL,".
+"sunmission_date DATE,".
+"FOREIGN KEY(student_ID) REFERENCES logintable(student_ID),".
+"FOREIGN KEY(coursework_ID) REFERENCES cwtable(coursework_ID),".
+"PRIMARY KEY(submission_ID))")or die("test".mysql_error());
+/*
+
+
+mysql_query("CREATE TABLE submissiontable(".
+"submission_ID INT NOT NULL AUTO_INCREMENT".
+"test VARCHAR(50) NOT NULL".
+//"student_ID INT NOT NULL,".
+//"coursework_ID INT NOT NULL,".
+//"submission_status BIT NOT NULL,".
+//"mark INT,".
+//"moderated BIT NOT NULL,".
+//"submission_date DATE,".
+"PRIMARY KEY(submission_ID))")or die("Could not add submissiontable: ".mysql_error());
+*/
+
+
+
+?>

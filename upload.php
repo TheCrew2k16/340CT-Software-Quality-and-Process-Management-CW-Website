a3bb15898f0db9d97bd5405ba00c3d495a26e781
 <sammy.whythim@gmail.com>
2016-12-14 21:06:08 +0000

    Fixed upload system to suit typical file types

--- upload.php
+++ upload.php
@@ -1,112 +1,70 @@
 <?php
-echo "start";
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["testname"]["name"]);
-$uploadOk = 1;
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-echo $target_dir;
-echo $target_file;
-if(isset($_POST["submit"])) {
-    echo "test";
-    $check = getimagesize($_FILES["testname"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-    }
-}
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
-}
-if ($_FILES["testname"]["size"] > 5000000000000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-    $uploadOk = 0;
-}
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-} else {
-    if (move_uploaded_file($_FILES["testname"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["testname"]["name"]). " has been uploaded.";
-    } else {
-        echo "Sorry, there was an error uploading your file.";
-    }
-}
+//Error reporting
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors', 1);
 
-/*
-
-
-
-
-        //turn on php error reporting
-        error_reporting(E_ALL | E_STRICT);
-        ini_set('display_errors', 1);
- 
-        if (isset($_FILES["testname"]["name"])) {
-             
-            $name     = $_FILES['testname']['name'];
-            $tmpName  = $_FILES['testname']['tmp_name'];
-            $error    = $_FILES['testname']['error'];
-            $size     = $_FILES['testname']['size'];
-            $ext      = strtolower(pathinfo($name, PATHINFO_EXTENSION));
-           
-            switch ($error) {
-                case UPLOAD_ERR_OK:
-                    $valid = true;
-                    //validate file extensions
-                    if ( !in_array($ext, array('jpg','jpeg','png','gif')) ) {
-                        $valid = false;
-                        $response = 'Invalid file extension.';
-                    }
-                    //validate file size
-                    if ( $size/1024/1024 > 2 ) {
-                        $valid = false;
-                        $response = 'File size is exceeding maximum allowed size.';
-                    }
-                    //upload file
-                    if ($valid) {
-                        $targetPath =  dirname( __FILE__ ) . DIRECTORY_SEPARATOR. 'uploads' . DIRECTORY_SEPARATOR. $name;
-                        move_uploaded_file($tmpName,$targetPath);
-                        header( 'Location: index.php' ) ;
-                        exit;
-                    }
-                    break;
-                case UPLOAD_ERR_INI_SIZE:
-                    $response = 'The uploaded file exceeds the upload_max_filesize directive in php.ini.';
-                    break;
-                case UPLOAD_ERR_FORM_SIZE:
-                    $response = 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form.';
-                    break;
-                case UPLOAD_ERR_PARTIAL:
-                    $response = 'The uploaded file was only partially uploaded.';
-                    break;
-                case UPLOAD_ERR_NO_FILE:
-                    $response = 'No file was uploaded.';
-                    break;
-                case UPLOAD_ERR_NO_TMP_DIR:
-                    $response = 'Missing a temporary folder. Introduced in PHP 4.3.10 and PHP 5.0.3.';
-                    break;
-                case UPLOAD_ERR_CANT_WRITE:
-                    $response = 'Failed to write file to disk. Introduced in PHP 5.1.0.';
-                    break;
-                case UPLOAD_ERR_EXTENSION:
-                    $response = 'File upload stopped by extension. Introduced in PHP 5.2.0.';
-                    break;
-                default:
-                    $response = 'Unknown error';
-                break;
+if (isset($_FILES["testname"]["name"])) {
+    
+    //Extractd file properties into variables
+    $name     = $_FILES['testname']['name'];
+    $tmpName  = $_FILES['testname']['tmp_name'];
+    $error    = $_FILES['testname']['error'];
+    $size     = $_FILES['testname']['size'];
+    $ext      = strtolower(pathinfo($name, PATHINFO_EXTENSION));
+    
+    //Swich statements determines whether to contintue or whether to throw up error messages
+    switch ($error) {
+        case UPLOAD_ERR_OK:
+            $valid = true;
+            //Checks file types
+            if ( !in_array($ext, array('doc','docx','odt','ods','odp','xls', 'xlsx','zip', 'ppt','pdf','pptx')) ) {
+                $valid = false;
+                $response = 'Invalid file extension.';
             }
+            //Checks file size
+            if ( $size/10000/10000 > 2 ) {
+                $valid = false;
+                $response = 'File size is exceeding maximum allowed size.';
+            }
+            //If deemed valid the file will be uploaded
+            if ($valid) {
+                $targetPath =  dirname( __FILE__ ) . DIRECTORY_SEPARATOR. 'uploads' . DIRECTORY_SEPARATOR. $name;
+                move_uploaded_file($tmpName,$targetPath);
+                header( 'Location: index.php' ) ;
+                exit;
+            }
+            break;
 
-            echo $response;
-        }
-        else{
-            echo "if skipped";
-            }*/
-        ?>
+        //Generic upload error procedures 
+        case UPLOAD_ERR_INI_SIZE:
+            $response = 'The uploaded file exceeds the upload_max_filesize directive in php.ini.';
+            break;
+        case UPLOAD_ERR_FORM_SIZE:
+            $response = 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form.';
+            break;
+        case UPLOAD_ERR_PARTIAL:
+            $response = 'The uploaded file was only partially uploaded.';
+            break;
+        case UPLOAD_ERR_NO_FILE:
+            $response = 'No file was uploaded.';
+            break;
+        case UPLOAD_ERR_NO_TMP_DIR:
+            $response = 'Missing a temporary folder. Introduced in PHP 4.3.10 and PHP 5.0.3.';
+            break;
+        case UPLOAD_ERR_CANT_WRITE:
+            $response = 'Failed to write file to disk. Introduced in PHP 5.1.0.';
+            break;
+        case UPLOAD_ERR_EXTENSION:
+            $response = 'File upload stopped by extension. Introduced in PHP 5.2.0.';
+            break;
+        default:
+            $response = 'Unknown error';
+        break;
+    }
+
+    echo $response;
+}
+else{
+    echo "if skipped";
+    }
+?>

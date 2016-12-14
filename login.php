b8891dbb09311e6d2e2eee78ead581e4bd301ec7
zTashii <tashman00@hotmail.co.uk>
2016-12-14 02:44:37 +0000

    Update login.php
    Added label entries for username and password (might pretty it up later)

--- login.php
+++ login.php
@@ -1,3 +1,6 @@
+<?php
+    session_start();
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -11,11 +14,21 @@
                 include "includes\header.php";
             ?>
         </div>
-        <div class="container-fluid" style="padding-left:100px;padding-right:100px;padding-top:20px;">
+        <div class="container-fluid" style="padding-left:200px;padding-right:100px;padding-top:200px;">
             <form>
-                
+                <p>
+                    <label>Username:</label>
+                    <input type="text" id="user" name="user"/>
+                </p>
+                <p>
+                    <label>Password:</label>
+                    <input type="password" id="pass" name="pass"/>
+                </p>
+                <p>
+                    <button type="submit">Login</button>
+                </p>
             </form>
         </div>  
     </body>
-
+    
 </html>

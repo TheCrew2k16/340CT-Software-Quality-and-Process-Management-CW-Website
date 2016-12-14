9bad5913c85c4b3d2c26b99d89a6e06a14776494
 <sammy.whythim@gmail.com>
2016-12-14 15:17:57 +0000

    Create basic submission UI

--- submitPage.php
+++ submitPage.php
@@ -11,6 +11,20 @@
                 include "includes\header.php";
             ?>
         </div>
+        <div class="container-fluid"style="padding-left:100px;padding-right:100px;padding-top:20px;">
+            <br><br><br><br>
+            <div class="jumbotron"> <!--creates a jumbotron contrainer that is a grey box -->
+                <form action="submitPage.php" method="GET">
+                    <h1>Submit Your Work</h1>
+                    <p>Enter your details and upload your Document</p>
+                    <div class="input-group">
+                        <label for="basic-url">Course Work Number:</p>
+                        <input class="form-control" type="text" name="courseWorkID" placeholder="Enter Course Work Number" required> <!-- makes a text box that is required -->
+                        <label class="control-label">Select File</label>
+                        <input  id="input-2" name="input2[]" type="file" class="file" multiple data-show-upload="false" data-show-caption="true">
+                    </div>
+
+        </div>
     </body>
 
 </html>

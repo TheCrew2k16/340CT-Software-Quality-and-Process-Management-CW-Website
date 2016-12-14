f817359d4d08661283a62767cd1d6a37197c0e3f
Philip Bell <philhabell@gmail.com>
2016-12-14 15:53:14 +0000

    added login check back

--- loginDetCheck.php
+++ loginDetCheck.php
@@ -11,13 +11,14 @@ $result = mysql_query($sql);
 while ($row = mysql_fetch_array($result)){
     echo $row['login_ID'];
 }
-/*
-if($row = mysql_fetch_assoc($result)){
+
+if($result){
+    echo "logged in";
     $action = "index.php";
     $logged = TRUE;
     $_SESSION[] = $row['login_ID'];
 } else{
     echo "Your username or password is incorrect!";
 }
-*/
+
 ?>

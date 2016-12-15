2413d09dd69d32d6bcfbcbd3229481d3b9073358
Philip Bell <philhabell@gmail.com>
2016-12-15 18:17:58 +0000

    moved james code into the correct file

--- cwStatus.php
+++ cwStatus.php
@@ -1,17 +1,43 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>CW Status Page</title>
-    </head>
-    <body>
-          <div>
-            <?php
-                $action = "status"; //sets the var $action to report, this will make the reports button on the nav bar be highlighted showing that the user is on the reports page. This happens in the header.php file
-                include "includes\boostrap.php"; //brings in the code that make the bootstrap css work 
-                include "includes\header.php"; //bring in the code that make the header that is on all pages of the site work
-            ?>
-        </div>
-        <div class="container-fluid" style="padding-left:100px;padding-right:100px;padding-top:20px;"> <!--makes a contrain with padding so the all the items on the page are not stretched to edge of the page -->
-
-    </body>
+	<head>
+		<title>Submission Status Page</title>
+	</head>
+	<body>
+		<div>
+			<?php
+				$action = "submission status";
+				include "includes\boostrap.php";
+				include "includes\header.php";
+			?>
+		</div>
+		
+		<div class="container-fluid" style="padding-left:100px;padding-top:20px;">
+			<br><br><br><br>
+			<h1>Submission Status</h1>
+			<br><br><br><br>
+			<table style="width:50%">
+				<tr>
+					<th>Coursework Title</th>
+					<th>Submitted</th>
+				</tr>
+				<tr>
+					<td>The Joy of Painting</td>
+					<td>Submitted</td>
+				</tr>
+				<tr>
+					<td>Art and design</td>
+					<td>Not Submitted</td>
+				</tr>
+				<tr>
+					<td>Cubic Art</td>
+					<td>Submitted</td>
+				</tr>
+			</table>
+			<?php
+				include "includes\dbconnect.php";
+				$sql = "SELECT * FROM submissiontable,cwTable WHERE submissiontable.coursework_ID=cwTable.coursework_ID";
+			?>
+		</div>
+	</body>
 </html>

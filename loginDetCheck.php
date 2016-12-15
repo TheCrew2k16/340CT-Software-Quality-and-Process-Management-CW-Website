ca560e2898d8055a755dbb95bf0c3f59e4a302c4
zTashii <tashman00@hotmail.co.uk>
2016-12-15 17:50:11 +0000

    Update loginDetCheck.php
    added role to the check

--- loginDetCheck.php
+++ loginDetCheck.php
@@ -4,23 +4,21 @@ session_start();
 include "includes\dbconnect.php";
 $uid = $_POST['uid'];
 $password = $_POST['pass'];
+$role = "";
 //retrieves the information from the login system and compares it with the input of the login
 $sql = "SELECT * FROM logintable WHERE login_ID = '$uid' AND pass = '$password'";
 $result = mysql_query($sql);
 while ($row = mysql_fetch_array($result)){
-    echo $row['login_ID'];
+    echo "redirecting you now. Login ID: ".$row['login_ID'];
     $_SESSION['login_ID'] = $row['login_ID'];
-    echo $row['login_ID'];
     if(isset($result)){
-
         $action = "index.php";
         $_SESSION['login_ID'] = $row['login_ID'];
         $row['login_ID'];
-        echo $_SESSION['login_ID'];
+        $_SESSION['role'] = $row['role'];
+        echo $role;
         header("refresh:5; url=index.php");
     } else{
         echo "Your username or password is incorrect!";
     }
-
 }
-
